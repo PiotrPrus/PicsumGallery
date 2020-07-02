b10af994b2e02440850c246d95fb28658854f941
piotrprus <piotr.prus@schibsted.com>
2020-07-03 00:44:54 +0200

    Add share icon, add colors and change app primaries manipulate detailView

--- app/src/main/java/com/piotrprus/picsumgallery/feature/detail/DetailFragment.kt
+++ app/src/main/java/com/piotrprus/picsumgallery/feature/detail/DetailFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import coil.api.load
 import com.piotrprus.picsumgallery.base.BaseFragment
 import com.piotrprus.picsumgallery.databinding.FragmentDetailBinding
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -11,6 +13,7 @@ import org.koin.android.viewmodel.ext.android.viewModel
 class DetailFragment : BaseFragment() {
 
     private val viewModel : DetailViewModel by viewModel()
+    private val args: DetailFragmentArgs by navArgs()
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
@@ -25,7 +28,8 @@ class DetailFragment : BaseFragment() {
     }
 
     override fun start() {
-
+        binding.detailFragmentAuthorName.text = args.picsum.author
+        binding.detailFragmentImage.load(args.picsum.downloadUrl)
     }
 
     override fun onDestroy() {
