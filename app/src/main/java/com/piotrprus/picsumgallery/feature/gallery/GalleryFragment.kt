f1c434e556b9835499bc4f8754df3e2732bcf271
piotrprus <piotr.prus@schibsted.com>
2020-07-02 22:46:02 +0200

    Modify GalleryFragment.kt to inherit from Fragment instead of BaseFragment due to usage of ViewBinding. Ideally ViewBinding should be initialize as generic in BaseFragment.

--- app/src/main/java/com/piotrprus/picsumgallery/feature/gallery/GalleryFragment.kt
+++ app/src/main/java/com/piotrprus/picsumgallery/feature/gallery/GalleryFragment.kt
@@ -1,16 +1,54 @@
 package com.piotrprus.picsumgallery.feature.gallery
 
-import com.piotrprus.picsumgallery.R
-import com.piotrprus.picsumgallery.base.BaseFragment
-import com.piotrprus.picsumgallery.base.LayoutResId
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.piotrprus.picsumgallery.databinding.FragmentGalleryBinding
+import com.piotrprus.picsumgallery.feature.gallery.recyclerview.PicsumAdapter
+import com.piotrprus.picsumgallery.feature.gallery.recyclerview.PicsumItemDecoration
 import org.koin.android.viewmodel.ext.android.viewModel
 
-@LayoutResId(R.layout.fragment_gallery)
-class GalleryFragment : BaseFragment() {
+class GalleryFragment : Fragment() {
 
     private val viewModel: GalleryViewModel by viewModel()
 
-    override fun start() {
-        super.start()
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val picsumAdapter = PicsumAdapter()
+        binding.picsumGridRV.apply {
+            adapter = picsumAdapter
+            addItemDecoration(PicsumItemDecoration())
+            layoutManager = GridLayoutManager(
+                requireContext(),
+                2,
+                RecyclerView.VERTICAL,
+                false
+            )
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
