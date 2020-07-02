70086b2aed3344399d11d3610c2f4f937290fff3
piotrprus <piotr.prus@schibsted.com>
2020-07-02 21:43:28 +0200

    Add PicsumAdapter.kt and PicsumViewHolder.kt that will be used in gallery recyclerView

---
+++ app/src/main/java/com/piotrprus/picsumgallery/feature/gallery/recyclerview/PicsumAdapter.kt
@@ -0,0 +1,33 @@
+package com.piotrprus.picsumgallery.feature.gallery.recyclerview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.piotrprus.picsumgallery.data.model.Picsum
+import com.piotrprus.picsumgallery.databinding.ItemPicsumBinding
+
+private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Picsum>() {
+    override fun areItemsTheSame(oldItem: Picsum, newItem: Picsum): Boolean =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: Picsum, newItem: Picsum): Boolean =
+        oldItem == newItem
+}
+
+class PicsumAdapter : ListAdapter<Picsum, PicsumViewHolder>(DIFF_CALLBACK) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PicsumViewHolder {
+        val binding = ItemPicsumBinding.inflate(
+            LayoutInflater.from(
+                parent.context
+            ),
+            parent,
+            false
+        )
+        return PicsumViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PicsumViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
---
+++ app/src/main/java/com/piotrprus/picsumgallery/feature/gallery/recyclerview/PicsumViewHolder.kt
@@ -0,0 +1,12 @@
+package com.piotrprus.picsumgallery.feature.gallery.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+import com.piotrprus.picsumgallery.data.model.Picsum
+import com.piotrprus.picsumgallery.databinding.ItemPicsumBinding
+
+class PicsumViewHolder(binding: ItemPicsumBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(picsum: Picsum) {
+        // TODO: Load image in imageView
+    }
+}
