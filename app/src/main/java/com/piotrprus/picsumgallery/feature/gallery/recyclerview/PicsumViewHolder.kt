2f0e3efb2cba876469b3c1b12d776bcde9fe4d94
piotrprus <piotr.prus@schibsted.com>
2020-07-02 23:14:39 +0200

    Add placeholder during image loading and crossFade animation

--- app/src/main/java/com/piotrprus/picsumgallery/feature/gallery/recyclerview/PicsumViewHolder.kt
+++ app/src/main/java/com/piotrprus/picsumgallery/feature/gallery/recyclerview/PicsumViewHolder.kt
@@ -2,12 +2,17 @@ package com.piotrprus.picsumgallery.feature.gallery.recyclerview
 
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
+import com.piotrprus.picsumgallery.R
 import com.piotrprus.picsumgallery.data.model.Picsum
 import com.piotrprus.picsumgallery.databinding.ItemPicsumBinding
 
-class PicsumViewHolder(private val binding: ItemPicsumBinding) : RecyclerView.ViewHolder(binding.root) {
+class PicsumViewHolder(private val binding: ItemPicsumBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 
     fun bind(picsum: Picsum) {
-        binding.itemPicsumImage.load(picsum.downloadUrl)
+        binding.itemPicsumImage.load(picsum.downloadUrl) {
+            placeholder(R.drawable.ic_placeholder)
+            crossfade(true)
+        }
     }
 }
